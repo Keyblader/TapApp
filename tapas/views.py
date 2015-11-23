513f669610bb9e3f3dc824a80fa7bc718e0080d4
Jordi Bordoy <i22bonij@uco.es>
2015-11-23 19:07:15 +0100

    modificar views para añadir tapa

--- tapas/views.py
+++ tapas/views.py
@@ -145,7 +145,7 @@ def anyadirTapa(request):
     """
     
     if request.method == 'POST':
-        serializer = TapaSerializer(data=request.data, files=request.FILES)
+        serializer = TapaSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
